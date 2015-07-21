b1abaa437fe26b912ea1588305045a314260859b
lightheat <lightandtheheat@gmail.com>
2015-07-21 02:11:57 -0400

    Add lab 3

---
+++ lab3src/lab03/MTQueue.java
@@ -0,0 +1,90 @@
+package lab03;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+
+import javax.swing.JOptionPane;
+
+import lab02.FantasyFootball;
+
+public class MTQueue {
+
+	protected final static int MAX_RECORD_LENGTH = 71;
+	protected final static int RECORD_COUNT = 20;
+	
+	public static String padLeft(String str, int x) {
+		return padLeft(str, x, ' ');
+	}
+
+	public static String padRight(String str, int x) {
+		return padRight(str, x, ' ');
+	}
+
+	public static String padZeros(String str, int x) {
+		return padLeft(str, x, '0');
+	}
+
+	public static String padLeft(String str, int x, char c) {
+		String padding = new String(new char[x - str.length()]).replace('\0',
+				c);
+		return (str.length() >= x) ? str : padding + str;
+	}
+
+	public static String padRight(String str, int x, char c) {
+		String padding = new String(new char[x - str.length()]).replace('\0',
+				c);
+		return (str.length() >= x) ? str : str + padding;
+	}
+
+	public static String repeatChar(int freq, char c) {
+		return new String(new char[freq]).replace('\0', c);
+	}
+	
+	private Semaphore _sem = new Semaphore(1);	
+	private Queue<String> _statQ;
+		
+	public MTQueue(){
+		_statQ = new LinkedList<String>();
+				
+	}
+	
+	public void MTPut(String qe){
+		try{
+			_sem.acquire();
+			_statQ.offer(qe);
+		} catch(InterruptedException ex){
+			System.out.println("Put interrupted.");
+		} finally {
+			_sem.release();
+		}
+	}
+	
+	public String MTGet(){
+		String retVal = new String();
+		try{
+			_sem.acquire();
+			retVal = _statQ.poll();
+		} catch(InterruptedException ex){
+			System.out.println("Get interrupted.");
+		} finally {
+			_sem.release();
+		}
+		return retVal;
+	}
+	
+	public static void main(String[] args) {
+		MTQueue master = new MTQueue();
+
+		Thread reading = new Thread(new ReadUserInput(master));
+		Thread writing = new Thread(new WriteToFile(master));
+		
+		reading.start();
+		writing.start();
+
+	}
+
+}
---
+++ lab3src/lab03/WriteToFile.java
@@ -0,0 +1,88 @@
+package lab03;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import javax.swing.JOptionPane;
+
+public class WriteToFile implements Runnable {
+	private MTQueue _cmdQ;
+	private static File _data;
+	private static RandomAccessFile _store;
+	private long _storeLength = 0;
+	
+	public WriteToFile(MTQueue mtq){
+		_cmdQ = mtq;
+		try {
+			if (_data == null){
+				_data = new File(System.getProperty("user.dir")
+						+ "\\datafiles\\lab02\\data.txt");
+			}
+			if (_store == null){
+				_store = new RandomAccessFile(_data, "rw");
+				_storeLength = _store.length();
+				if (_storeLength < ((MTQueue.MAX_RECORD_LENGTH + 2) * MTQueue.RECORD_COUNT)) {
+					for (int i = 0; i < 20; ++i) {
+						try {
+							_store.writeUTF(MTQueue.repeatChar(71, ' '));
+						} catch (IOException e) {
+							JOptionPane.showMessageDialog(null,
+									"There was a problem writing to the file: "
+											+ e.getMessage(),
+									"Uh oh", JOptionPane.ERROR_MESSAGE);
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null,
+					"There was a problem opening the file for writing: "
+							+ e.getMessage(),
+					"Uh oh", JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
+		}
+	}
+	
+	@Override
+	public void run() {
+		while (true){
+			String strToExecute = _cmdQ.MTGet();
+			if (strToExecute == null){
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				continue;
+			} else if (strToExecute.compareToIgnoreCase("end") == 0){
+				break;
+			}
+			String idPortion = strToExecute.substring(0, 5);
+			int intId = 0;
+			try{
+				intId = Integer.parseInt(idPortion.trim());
+			} catch(NumberFormatException e){
+				e.printStackTrace();
+			}
+			
+			int bytePos = intId > 0 ? (intId - 1) * MTQueue.MAX_RECORD_LENGTH : 0;
+
+			try {
+				_store.seek(bytePos);
+				_store.writeUTF(strToExecute);
+				_store.seek(bytePos);
+				System.out.println(_store.readUTF());
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null,
+						"There was a problem writing to the file: "
+								+ e.getMessage(),
+						"Uh oh", JOptionPane.ERROR_MESSAGE);
+			}
+			
+		}
+		
+
+	}
+
+}
